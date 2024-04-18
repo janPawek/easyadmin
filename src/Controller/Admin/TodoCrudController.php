13be9c536d60e170a5142c261825d3e6c294ba62
Jan <jan.pawek@outlook.com>
2024-04-18 11:58:00 +0200

    add

--- src/Controller/Admin/TodoCrudController.php
+++ src/Controller/Admin/TodoCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Todo;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -21,6 +22,7 @@ class TodoCrudController extends AbstractCrudController
         return [
             TextField::new('name'),
             TextEditorField::new('description'),
+            ImageField::new('picture')->setBasePath('uploads/pictures/')->setUploadDir('public/uploads/pictures/')->setUploadedFileNamePattern('[slug]-[uuid].[extension]'),
             AssociationField::new('status')->setFormTypeOption('choice_label', function ($choice) {
                 return $choice->getName();
             })
